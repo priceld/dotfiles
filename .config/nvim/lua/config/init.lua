708af9266a3ccddcc6541aaea01a2426918f3ac7
Logan Price <logan.price@anthology.com>
2023-12-08 14:40:12 -0500

    feat: my initial nvim config

---
+++ .config/nvim/lua/config/init.lua
@@ -0,0 +1,31 @@
+require("config.lazy")
+require("config.options")
+require("config.remap")
+
+local my_group = vim.api.nvim_create_augroup("LPConfig", {})
+local yank_group = vim.api.nvim_create_augroup("HighlightYank", {})
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+	group = yank_group,
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank({
+			higroup = "IncSearch",
+			timeout = 40,
+		})
+	end,
+})
+
+-- This if from the Primeagen's config and I think it just removes
+-- traling spaces before save, which I probably don't need thanks
+-- to prettier, but it is probably safe to leave for the files that
+-- prettier does not modify.
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+	group = my_group,
+	pattern = "*",
+	command = [[%s/\s\+$//e]],
+})
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,46 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+-- Yank, delete, change should copy to system clipboard.
+vim.opt.clipboard = "unnamedplus"
+
+-- TODO: figure out what I want here. Not sure about swap/backup,
+-- but I think I want undodir...but it should probably take the NVIM_APPNAME
+-- into account?
+--
+-- vim.opt.swapfile = false
+-- vim.opt.backup = false
+-- vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+-- vim.opt.undofile = true
+
+vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.termguicolors = true
+
+-- Keeps the cursor in the middle of the screen except at the top/bottom of buffers.
+-- Seems to work better (for scrolling) the answer given here:
+-- https://stackoverflow.com/questions/13398631/always-keep-the-cursor-centered-in-vim/63326139#63326139
+vim.opt.scrolloff = 999
+
+vim.opt.virtualedit = "block"
+
+-- vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "120"
+vim.opt.signcolumn = "yes"
+
